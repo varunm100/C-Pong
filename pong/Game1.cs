91005b166c85842a5dbd914381cb7e5bb9886264
Varun Munagala <varunm100@gmail.com>
2018-01-01 13:43:33 +0530

    Add project files.

---
+++ pong/Game1.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace pong
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Paddle paddleLeft;
+        Paddle paddleRight;
+        Paddle paddleTop;
+        Paddle paddleBottom;
+
+        Texture2D paddleTexture;
+        Vector2 paddleScale;
+        float paddleSca = 25f;
+
+        Texture2D ballTexture;
+        Vector2 ballScale;
+        Vector2 ballPosition;
+        Vector2 ballVelocity = new Vector2(125,50);
+        public static Vector2 ballInitialVelocity = new Vector2(125, 50);
+        float PaddleSpeedScaleFac = 1.5f;
+        
+        float ballSca = 25f;
+        Rectangle ballCollider;
+
+        float ScreenWidth;
+        float ScreenHeight;
+
+        public static int TurnCount = 0;
+
+        Texture2D pongTable;
+        Vector2 tableScale;
+
+        SpriteFont font;
+        bool gameOver = false;
+        bool LeftWin;
+        bool displayWinner = false;
+        String winningString;
+        
+        public static float count = 1;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            ScreenWidth = GraphicsDevice.PresentationParameters.Bounds.Width;
+            ScreenHeight = GraphicsDevice.PresentationParameters.Bounds.Height;
+            ballPosition = new Vector2(ScreenWidth/2, ScreenHeight/2);
+            ballVelocity *= PaddleSpeedScaleFac;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            paddleTexture = this.Content.Load<Texture2D>("pong-paddle");
+            paddleScale = new Vector2(paddleSca / paddleTexture.Width, paddleSca / paddleTexture.Width);
+
+            ballTexture = this.Content.Load<Texture2D>("pong-ball");
+            ballScale = new Vector2(ballSca / ballTexture.Width, ballSca / paddleTexture.Width);
+
+            paddleLeft = new Paddle(paddleTexture, new Vector2(0, 0), paddleScale, 300, 0);
+            paddleRight = new Paddle(paddleTexture, new Vector2(ScreenWidth-(paddleTexture.Bounds.Width*paddleScale.X), 0), paddleScale, 300, 0);
+
+            pongTable = this.Content.Load<Texture2D>("pong-table");
+            tableScale = new Vector2(ScreenWidth / pongTable.Width, ScreenHeight / pongTable.Height);
+
+            font = this.Content.Load<SpriteFont>("pong-score-font");
+        }
+
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            ballCollider = new Rectangle((int)ballPosition.X, (int)ballPosition.Y, (int)ballTexture.Width*(int)ballScale.X, (int)ballTexture.Height*(int)ballScale.Y);
+
+            paddleLeft.MoveUpADown(Keys.A,Keys.D,gameTime);
+            paddleLeft.CheckOutBounds(GraphicsDevice.PresentationParameters.Bounds.Width, GraphicsDevice.PresentationParameters.Bounds.Height);
+            ballVelocity = paddleLeft.HandleBallCollision(ballCollider, ballVelocity, ballPosition, 1, -1);
+
+            paddleRight.MoveUpADown(Keys.NumPad4, Keys.NumPad6, gameTime);
+            paddleRight.CheckOutBounds(GraphicsDevice.PresentationParameters.Bounds.Width, GraphicsDevice.PresentationParameters.Bounds.Height);
+            ballVelocity = paddleRight.HandleBallCollision(ballCollider, ballVelocity, ballPosition, -1, 1);
+
+            ballPosition += ballVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (ballPosition.Y <= 0)
+            {
+                ballVelocity.Y *= (float)-0.9;
+                ballVelocity.X *= (float)1.05;
+            }
+            else if (ballPosition.Y >= ScreenHeight)
+            {
+                ballVelocity.Y *= (float)-0.9;
+                ballVelocity.X *= (float)1.05;
+            }
+            if (ballPosition.X < 0)
+            {
+                Console.WriteLine("Game Over! Right Pong Player Won In " + TurnCount.ToString() + " turns");
+                winningString = "Right Pong Player Wins!";
+                gameOver = true;
+                LeftWin = false;
+                TurnCount = 0;
+                displayWinner = true;
+            } else if (ballPosition.X > ScreenWidth)
+            {
+                Console.WriteLine("Game Over! Left Pong Player Won In " + TurnCount.ToString() + " turns");
+                winningString = "Left Pong Player Wins!";
+                gameOver = true;
+                LeftWin = true;
+                TurnCount = 0;
+                displayWinner = true;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.R) && gameOver)
+            {
+                gameOver = false;
+                ballPosition = new Vector2(ScreenWidth / 2, ScreenHeight / 2);
+                ballVelocity = ballInitialVelocity;
+                ballVelocity *= PaddleSpeedScaleFac;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            
+            spriteBatch.Begin();
+            spriteBatch.Draw(pongTable, position: Vector2.Zero, scale: tableScale);
+            spriteBatch.DrawString(font, TurnCount.ToString(), new Vector2(paddleScale.X*paddleTexture.Width+20, ScreenHeight-45), Color.Black);
+            spriteBatch.Draw(paddleRight.paddleTexture, position: paddleRight.position, scale: paddleRight.scale, rotation: paddleRight.rotation, origin: paddleRight.origin);
+            spriteBatch.Draw(paddleLeft.paddleTexture, position: paddleLeft.position, scale: paddleLeft.scale, rotation: paddleLeft.rotation, origin: paddleLeft.origin);
+            if (displayWinner) { spriteBatch.DrawString(font, winningString, new Vector2(ScreenWidth / 2, ScreenHeight / 2), Color.Black); displayWinner = false; }
+            if (!gameOver) { spriteBatch.Draw(ballTexture, position: ballPosition, scale: ballScale); }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ pong/Paddle.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace pong
+{
+    class Paddle
+    {
+        public Texture2D paddleTexture;
+        public Vector2 position;
+        public Vector2 scale;
+        public Vector2 origin;
+        public float moveSpeed;
+        public float rotation;
+        public float paddleHeight;
+        public Vector2 paddleVelocity;
+
+        public Paddle(Texture2D _paddleTexture, Vector2 _position, Vector2 _scale, float _moveSpeed, float _rotation)
+        {
+            this.moveSpeed = _moveSpeed;
+            this.paddleTexture = _paddleTexture;
+            this.position = _position;
+            this.scale = _scale;
+            this.rotation = _rotation;
+        }
+
+        public void UpdateOrigin()
+        {
+            origin = new Vector2((position.X + (paddleTexture.Bounds.Width / 2)), (position.Y + (paddleTexture.Bounds.Height/2)));
+        }
+
+        public void MoveUpADown(Keys up, Keys down, GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(up))
+            {
+                position += new Vector2(0, -moveSpeed) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                paddleVelocity = new Vector2(0, -moveSpeed) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            } else if (state.IsKeyDown(down))
+            {
+                position += new Vector2(0, moveSpeed) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                paddleVelocity = new Vector2(0, moveSpeed) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+
+        public void MoveLeftARight(Keys left, Keys right, GameTime gameTime) {
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(left))
+            {
+                position += new Vector2(-moveSpeed, 0) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                paddleVelocity = new Vector2(-moveSpeed, 0) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            } else if (state.IsKeyDown(right))
+            {
+                position += new Vector2(moveSpeed, 0) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                paddleVelocity = new Vector2(moveSpeed, 0) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+
+        public void CheckOutBounds(float windowWidth, float windowHeight)
+        {
+            if (position.X < 0)
+            {
+                position.X = 0;
+            }
+            if (position.X > windowWidth)
+            {
+                position.X = windowWidth;
+            }
+            if (position.Y < 0)
+            {
+                position.Y = 0;
+            }
+            if (position.Y > windowHeight-(scale.Y*paddleTexture.Height))
+            {
+                position.Y = windowHeight - (scale.Y * paddleTexture.Height);
+            }
+        }
+
+        public Vector2 HandleBallCollision(Rectangle Ball, Vector2 ballVelocity, Vector2 ballPosition, int ScaleCos, int ScaleSin)
+        {
+            Rectangle PaddleBoundingBox = new Rectangle((int)position.X, (int)position.Y, (int)(scale.X * paddleTexture.Width), (int)(scale.Y * paddleTexture.Height));
+            paddleHeight = paddleTexture.Height * scale.Y;
+            if (PaddleBoundingBox.Intersects(Ball))
+            {
+                //Console.WriteLine("Collision Detected!");
+                float YIntersection = (position.Y + (paddleHeight / 2)) - ballPosition.Y;
+                float NormalizedYIntersect = (YIntersection / (paddleHeight / 2));
+                float bounceAngle = NormalizedYIntersect * 75;
+                ballVelocity = new Vector2((float)(ScaleCos*600*Game1.count*Math.Cos(ConvertDegToRad(bounceAngle))), (float)(ScaleSin*600*Math.Sin(ConvertDegToRad(bounceAngle))));
+                Game1.TurnCount += 1;
+                //Game1.count *= 1.5f;
+                Console.WriteLine(bounceAngle);
+                return ballVelocity;
+            } else
+            {
+                return ballVelocity;
+            }
+        }
+
+        public static float ConvertDegToRad(float deg)
+        {
+            return (float)Math.PI * (float)deg / 180.0f;
+        }
+    }
+}
